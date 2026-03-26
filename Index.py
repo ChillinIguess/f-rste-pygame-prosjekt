633fd0f424a217b3c08e40fd73b4dc69ef371cfa
ChillinIguess <caspardbt2009@gmail.com>
2026-03-26 15:17:27 +0100

    Forandret på noen verdier

---
+++ Index.py
@@ -0,0 +1,190 @@
+import pygame
+from sys import exit
+import os
+
+#Variables for the screen
+gameWidth = 1280
+gameHeight = 720
+
+#Game variables
+playerX = gameWidth/2
+playerY = gameHeight/2
+playerWidth = 40
+playerHeight = 40
+playerDistance = 5
+size = 40
+kanHoppe = False
+turnedRight = True
+#Gravity
+playerVelocityY = 0
+gravity = 0.5
+jump_strength = -12
+on_ground = False
+gravityplus = 3
+fastFall = 1
+ground_y = gameHeight
+#Glidning
+playerVelocityX = 0
+sliding = 0.5
+
+#colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+green = (0, 255, 0)
+pygame.init()
+
+'''platform = pygame.rect(10, 100, 10)
+pygame.draw.platform'''
+
+#Window variables 
+clock = pygame.time.Clock()
+window = pygame.display.set_mode((gameWidth, gameHeight))
+tileSize = ((gameWidth + gameHeight)/2/10)
+'''
+def draw_grid():
+    for line in range(0, 6):
+        pygame.draw.line(window, (255, 255, 255), (0, line * tileSize), (gameWidth, line * tileSize))
+        pygame.draw.line(window, (255, 255, 255), (0, line * tileSize, 0), (line * tileSize, gameHeight))
+
+class platform():
+
+    
+ 
+
+class world():
+    def __init__(self, data):
+        ground = (white)
+
+        for row in data:
+            for tile in row:
+                if tile == 1:
+                    ground 
+
+worldData = [1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1],
+
+draw_grid()
+'''
+#Defines player
+class Player():
+    def __init__(self,x,y,size):
+      self.x = x
+      self.y = y
+      self.size = size
+      self.velocity_y = 0
+      self.playerRect = pygame.Rect(self.x, self.y, self.size,self.size)
+    def playerMove(self):
+         self.playerRect = pygame.Rect(self.x, self.y, self.size,self.size)
+    def draw(self):
+        pygame.draw.rect(window, (green), self.playerRect)
+#Defines enemy
+class Enemy(Player):
+    def __init__(self,x,y,size):
+        Player.__init__(self,x,y,size)
+player = Player(playerX, playerY, playerWidth)
+e = Enemy(111,111,50)
+
+
+
+
+pygame.display.update()
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+             # Jump
+       
+
+    # Apply gravity
+    
+    playerVelocityY += gravity
+    player.y += playerVelocityY    
+    
+   
+    
+    #PLAYER_VELOCITY_X = sliding
+    #player.x = PLAYER_VELOCITY_X
+
+    # Collision with ground
+    if player.y + playerHeight >= ground_y:
+        player.y = ground_y - playerHeight
+        playerVelocityY = 0
+        on_ground = True
+        kanHoppe = True
+    
+
+    window.fill(black)
+    
+    '''if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d or event.key == pygame.K_RIGHT: 
+                player.x += PLAYER_DISTANCE
+            
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT: 
+                player.x -= PLAYER_DISTANCE '''
+                
+
+        
+    keys = pygame.key.get_pressed()
+    
+    
+    #Jumping
+    if keys[pygame.K_SPACE] or keys[pygame.K_UP]:
+        if kanHoppe:
+            playerVelocityY = jump_strength 
+            on_ground = False 
+            kanHoppe = False    
+              
+            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+                playerVelocityY = jump_strength + gravityplus
+            if keys[pygame.K_w] or keys[pygame.K_UP]:
+                playerVelocityY = jump_strength - gravityplus 
+          
+          #Faster fall downwards 
+    if playerVelocityY > 2 and playerVelocityY <15:
+                playerVelocityY += fastFall
+
+    
+         
+        
+    
+   
+    
+    
+           #Left movement
+
+        #if PLAYER_VELOCITY_Y <= jump_strength/3 and (keys[pygame.K_s] or keys[pygame.K_DOWN]): PLAYER_VELOCITY_Y = jump_strength + gravityplus
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and player.x - playerDistance >= 0:
+       player.x -= playerDistance
+       turnedRight = False
+   
+       '''if player.x >= -5:
+            PLAYER_VELOCITY_X -= PLAYER_DISTANCE
+            player.x -=PLAYER_VELOCITY_X'''
+       '''if keys [pygame.K_LEFT] or keys[pygame.K_a] == False:
+            if player.x >= 0: 
+                player.x -= PLAYER_DISTANCE - sliding'''
+         
+        #Right movement
+        
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d] and player.x + playerDistance <= gameWidth - playerWidth:
+        player.x += playerDistance
+        turnedRight = True
+
+        ''' if player.x <= 5:
+            PLAYER_VELOCITY_X += PLAYER_DISTANCE
+            player.x += PLAYER_VELOCITY_X'''
+       
+
+#Variables
+    player.playerMove()
+    player.draw()
+    e.draw()
+    pygame.display.update()
+    clock.tick(60)
+        
+
+       
